6d6341a28a23c046137d61900b8b1a0d4ab1cb31
Ravi Le <ravi.le@transwap.com>
2021-11-26 02:50:07 +0700

    setup basic express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello from my devconnector")
+const express = require("express")
+
+const app = express()
+
+
+app.use(express.json())
+
+
+const port = process.env.Port || 3000
+
+app.get("/", async (req, res) => {
+    res.send("hello")
+})
+
+
+app.listen(port, () => {
+    console.log("server is listening to port " + port)
+})
